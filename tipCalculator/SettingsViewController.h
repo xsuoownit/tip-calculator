53e9165f0f6fea238c983c20f2ce4024e53fb5d8
Xin Suo <xsuo@groupon.com>
2015-10-04 13:25:58 -0700

    add new settings view controller

---
+++ tipCalculator/SettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.h
+//  tipCalculator
+//
+//  Created by Xin Suo on 10/4/15.
+//  Copyright © 2015 Groupon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UIViewController
+
+@end
