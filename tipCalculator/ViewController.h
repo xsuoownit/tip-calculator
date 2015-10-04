5c1866f7982c0e0a7df06d56af5d7091bd46cd3b
Xin Suo <xsuo@groupon.com>
2015-10-04 11:29:48 -0700

    Initial Commit

---
+++ tipCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tipCalculator
+//
+//  Created by Xin Suo on 10/4/15.
+//  Copyright © 2015 Groupon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ tipCalculator/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  tipCalculator
+//
+//  Created by Xin Suo on 10/4/15.
+//  Copyright © 2015 Groupon. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
